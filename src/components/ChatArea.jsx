47a39bb81fc23d358d04412eb26e7245dbadf6f4
VikasGgit <vikas.college001@gmail.com>
2025-04-26 13:40:20 +0530

    chat area top reduced

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -96,7 +96,7 @@ const ChatArea = ({ chatUser }) => {
 
 
   return (
-    <div className={`fixed top-52 right-4 ${chatView ? "slide-in-right" : "slide-out-right"}`}>
+    <div className={`fixed top-30 right-4 ${chatView ? "slide-in-right" : "slide-out-right"}`}>
 
       <Box sx={{ display: 'flex', flexDirection: 'column', height: '500px', width: '400px', border: '1px solid #ccc', borderRadius: 2 }}>
         {/* Header */}
